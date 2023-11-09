89661049d11382875b189d6e2839ec00acbcf4ef
Ben Mordue <benmordue@gmail.com>
2023-11-09 12:22:13 +0000

    bad config?

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,13 +1,15 @@
 // stryker.conf.js
 
 const config = {
-  mutate: ["lib/**/*.ts"],
+  mutate: ["lib/**/*.ts", "!lib/Hex.ts"],
   testRunner: "mocha",
   reporters: ["progress", "clear-text", "html", "dashboard"],
   coverageAnalysis: "perTest",
   mochaOptions: {
     spec: [ "built/test" ]
-  }
+  },
+  disableTypeChecks: false,
+  concurrency: 2
 };
 
 module.exports = config;
