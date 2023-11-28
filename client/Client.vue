2edc03383c6a7e1c9e0251c3d5582902c34e2f69
Ben Mordue <BMordue@hymans.co.uk>
2023-11-28 16:20:16 +0000

    vuejs initial scaffold

---
+++ client/Client.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+const message = ref('Hello world')
+
+const counter = reactive({ count: 0 })
+
+counter.count++
+
+// console.log(message.value)
+
+message.value = 'Changed'
+
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+    <p>count is {{ counter.count }}</p>
+</template>
