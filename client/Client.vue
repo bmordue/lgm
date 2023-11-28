12ee6be472198a526a424787f5f84bc50f1f7664
Ben Mordue <benmordue@gmail.com>
2023-11-28 21:27:15 +0000

    wip

--- client/Client.vue
+++ client/Client.vue
@@ -7,6 +7,14 @@ const API_URL = "http://localhost:5000/listGames"
 watchEffect(async () => {
   gameList.value = await (await fetch(API_URL)).json().gameIds;
 });
+  
+async function callCreate() {
+async function logMovies() {
+  const response = await fetch("http://example.com/movies.json");
+  const movies = await response.json();
+  console.log(movies);
+}
+
 
 </script>
 
