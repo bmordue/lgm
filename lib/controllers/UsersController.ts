1c0a73bd08d61f7eb24ce8bf0fe062edca4c80de
Ben Mordue <benmordue@gmail.com>
2024-02-27 20:43:37 +0000

    fix up sweeped users controller

---
+++ lib/controllers/UsersController.ts
@@ -0,0 +1,16 @@
+import { ExegesisContext } from "exegesis";
+import { Request, Response } from 'express';
+import * as crypto from 'crypto';
+
+module.exports.loginUser = function (context: ExegesisContext): void {
+  const { username, password } = context.requestBody;
+
+  if (!username || !password) {
+    context.res.status(400).json({ message: 'Username and password are required.' });
+    return;
+  }
+
+  const token = crypto.createHash('sha256').update(username).digest('hex');
+  context.res.status(200).json({ token });
+}
+
